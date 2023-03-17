b20a972d3dee96cc97939df16d0db78691bbe9bd
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-03-17 13:33:57 -0400

    feat: initial dropdown navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,14 +1,51 @@
-import { NavbarContainer, NavbarContent, Logo, DropdownButton } from "./styles";
+import { NavbarContainer, NavbarContent, Logo, DropdownButton, Pages, PagesContainer } from "./styles";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const variants = {
+    open: { opacity: 1 },
+    closed: { opacity: 0 },
+}
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleDropdownButton = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
-        <NavbarContainer>
-            <NavbarContent>
-                <Logo />
-                <h2>Home</h2>
-                <DropdownButton />
-            </NavbarContent>
-        </NavbarContainer>
+        <>
+            {
+                isOpen ?
+                    <PagesContainer>
+                        <Pages
+                            initial={{ x: '100%' }}
+                            animate={{ x: 0 }}
+                            transition={{
+                                duration: 0.2,
+                                type: "tween"
+                            }}
+                        >
+                            <button onClick={handleDropdownButton}>x</button>
+                            <h1>Opa</h1>
+                            <h1>Opa</h1>
+                            <h1>Opa</h1>
+                            <h1>Opa</h1>
+                        </Pages>
+                    </PagesContainer>
+                    :
+                    null
+            }
+            <NavbarContainer>
+                <NavbarContent>
+                    <Logo />
+                    <h2>Home</h2>
+                    <DropdownButton onClick={handleDropdownButton} />
+
+                </NavbarContent>
+            </NavbarContainer>
+        </>
     )
 }
 
--- frontend/src/components/Navbar/styles.ts
+++ frontend/src/components/Navbar/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { DefaultContainer, DefaultContent } from "../../styles/global";
 import { ReactComponent as LogoIcon } from "../../assets/logo_1.svg";
 import { ReactComponent as DropdownIcon } from "../../assets/mobile_dropdown_button_1.svg";
+import { motion } from "framer-motion";
 
 export const NavbarContainer = styled(DefaultContainer)`
     height: 85px;
@@ -14,11 +15,12 @@ export const NavbarContent = styled(DefaultContent)`
     align-items: center;
     color: var(--whiteFixed);
     font: var(--Heading-5-400);
+    text-decoration-line: underline;
 `
 
 export const Logo = styled(LogoIcon)`
-    width: 25px;
-    height: 25px;
+    width: 30px;
+    height: 30px;
     color: #ffffff;
 `
 
@@ -26,5 +28,28 @@ export const DropdownButton = styled(DropdownIcon)`
     width: 25px;
     height: 25px;
     color: #ffffff;
+    cursor: pointer;
+
+    :hover {
+        cursor: pointer;
+    }
+`
+
+export const PagesContainer = styled.div`
+    display: flex;
+    justify-content: end;
+    position: absolute;
+    width: 100%;
+    min-height: 100vh;
+    background-color: #000000aa;
+`
+
+export const Pages = styled(motion.div)`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 70%;
+    height: 100vh;
+    background-color: var(--whiteFixed);
 `
 
